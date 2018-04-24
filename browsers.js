5aa2fef3834f904bd0edcd457824be0146699b73
leonardo rojas <leonardo.rojas@shopify.com>
2018-04-24 11:43:08 -0400

    Constraint list exclude defaults
    - Explicitly add last 3 version of Edge
    - Replace android (web view) with Chrome for android
    
    This gets support to just under 72% of [global coverage]
    
    [global coverage]: http://browserl.ist/?q=last+3+chrome+versions%2C+last+3+firefox+versions%2C+last+3+chromeandroid+versions%2C+last+2+edge+versions%2C+safari+%3E%3D+10%2C+ios+%3E%3D+10

--- browsers.js
+++ browsers.js
@@ -1,8 +1,8 @@
 module.exports = [
   'last 3 chrome versions',
   'last 3 firefox versions',
-  'last 2 versions',
+  'last 3 edge versions',
   'safari >= 10',
   'ios >= 10',
-  'android >= 6',
+  'last 3 chromeAndroid',
 ];
